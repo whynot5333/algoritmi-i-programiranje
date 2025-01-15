a5235334de36041e35d54d0cad772c1a972e470a
whynot5333 <pevecvito09@gmail.com>
2025-01-15 11:26:13 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    int rezultat;
+    printf("unesi slovo");
+    cin>>a;
+    rezultat=a;
+    printf("Slovo je broj: %i", rezultat);
+    return 0;
+}
