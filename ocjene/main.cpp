c1fadb25c3738c7714593e6683362c5a0b9ba467
whynot5333 <pevecvito09@gmail.com>
2025-02-05 11:21:07 +0100

    Add files via upload

---
+++ ocjene/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ /* int b;
+  cin>>b;
+
+  int B5;
+  cin>>B5;
+
+  int B4;
+  cin>>B4;
+
+  int B3;
+  cin>>B3;
+
+  int B2;
+  cin>>B2;
+
+    if(b>=B5)
+        cout<<"Odlican";
+
+    else if(b>=B4)
+        cout<<"vrlo dobra";
+
+    else if (b>=B3)
+        cout<<"dobar";
+
+    else if(b>=B2)
+        cout<<"dovoljan";
+
+    else
+        cout<<"nedovoljan";
+
+    return 0;
+}
+*/
+/* int bezPDV;
+cin>> bezPDV;
+float sPDV;
+sPDV=bezPDV+bezPDV*0.25;
+cout<<"S PDV-om: "<<sPDV ;
+return 0;
+}
+*/
+/*int b;
+cin>>b;
+if (b>=0)
+    cout<<("pozitivan");
+    else
+    cout<<("negativan");
+    return 0;
+}
+*/
+
+
+
+char odabir;
+int v1, v2;
+scanf("%c %i %i", &odabir, &v1, &v2);
+if (odabir=='R'){
+int U=v1;
+int I=v2;
+int R=U/I;}
+else if(odabir=='U'){
+int U=v1;
+int I=v2;
+int R=U/I;
+cout<<U;}
+else if(odabir=='I'){
+    int U=v1;
+    int I=v2;
+    int R=U/I;
+    cout<<I;}
+
+return 0;}
+
+
+
+
+
+
+
+
+
+
