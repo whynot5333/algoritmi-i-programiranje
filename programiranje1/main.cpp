9dd83c15a7c6d940455d52eb8373836acd7d8695
whynot5333 <pevecvito09@gmail.com>
2025-02-07 19:29:55 +0100

    Add files via upload

--- programiranje1/main.cpp
+++ programiranje1/main.cpp
@@ -4,15 +4,76 @@ using namespace std;
 
 int main()
 {
-   char slovo1;
-   char slovo2;
-   int rezultat;
 
-   printf("unesi prvo slovo: ");
-   cin>>slovo1;
+/*70 30 40 60
+cout<<"upiši id";
+int id;
+cin>>id;
+if (id<=70){
+    cout<<"razred 1";}
+else if (id>=71 && id<=100){
+    cout<<"razred 2";}
+else if( id>=101 && id<=140){
+    cout<<"razred 3";}
+else if (id>=141 && id <=200){
+    cout<<"razred 4";}
 
-   printf("unesi drugo slovo: ");
-   cin>>slovo2;
-   rezultat=slovo1-slovo2;
-   printf("Razmak izmedju brojeva %i", rezultat);
+*/
+/*
+int a,b,c;
+cout<<"upiši tri str"<<endl;
+cin>>a;
+cin>>b;
+cin>>c;
+if(a>b && a>c ){
+    if(b>a/2 && c>a/2){
+    int opseg;
+    opseg=a+b+c;
+    cout<<"opseg"<<opseg;
+        }
+    }
+
+else if(b>a && b>c ){
+    if(a>b/2 && c>b/2){
+    int opseg;
+    opseg=a+b+c;
+    cout<<"opseg"<<opseg;
+        }
+    }
+else if(c>a && c>b ){
+    if(a>c/2 && b>c/2){
+    int opseg;
+    opseg=a+b+c;
+    cout<<"opseg"<<opseg;
+        }
+    }
+*/
+
+
+
+
+char slovo;
+cin>>slovo;
+if(slovo=='a'){
+    cout<<"Sjeverna Amerika,Juzna Amerika,Afrika,Europa";}
+else if(slovo=='i'){
+    cout<<"Afrika,Azija,Australija";}
+else if(slovo=='t'){
+    cout<<"Sjeverna Amerika,Juzna Amerika";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
 }
